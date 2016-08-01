6018b4579a43c4b9abcc1ddd10b8142092b8e375
TButlerDEV <tbutler.it@gmail.com>
2016-08-01 11:20:33 -0400

    ManageUserPartialError

--- MVC5RealWorld/Controllers/HomeController.cs
+++ MVC5RealWorld/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Web.Mvc;
 using MVC5RealWorld.Security;
+using System.Web.Security;
+using MVC5RealWorld.Models.ViewModel;
+using MVC5RealWorld.Models.EntityManager;
 
 namespace MVC5RealWorld.Controllers
 {
@@ -24,6 +27,20 @@ namespace MVC5RealWorld.Controllers
 
         public ActionResult UnAuthorized()
         {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager();
+                UserDataView UDV = UM.GetUserDataView(loginName);
+                return PartialView(UDV);
+            }
+
             return View();
         }
     }
